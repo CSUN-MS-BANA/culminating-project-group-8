bde7f78540f7539940186ae44ca302a69bdd41db
rutvikbhatia <159727428+rutvikbhatia@users.noreply.github.com>
2025-02-23 16:47:56 -0800

    Add files via upload

---
+++ Temp/Cleaned_Global_Indicators_New.sql
@@ -0,0 +1,228 @@
+
+CREATE TABLE Cleaned_Global_Indicators_New (
+    Country_Name INT,
+    Country_Code INT,
+    Series_Name INT,
+    Series_Code INT,
+    Year_2021 FLOAT,
+    Year_2022 FLOAT,
+    Year_2023 FLOAT,
+    Series_Name_Social INT,
+    Series_Code_Social INT,
+    Year_2021_Social FLOAT,
+    Year_2022_Social FLOAT,
+    Year_2023_Social FLOAT,
+    Series_Name_Env INT,
+    Series_Code_Env INT,
+    Year_2021_Env FLOAT,
+    Year_2022_Env FLOAT,
+    Year_2023_Env FLOAT,
+    Series_Name_Stats INT,
+    Series_Code_Stats INT,
+    Year_2021_Stats FLOAT,
+    Year_2022_Stats FLOAT,
+    Year_2023_Stats FLOAT,
+    Country_ID VARCHAR(10),
+    Name INT
+);
+
+    INSERT INTO Cleaned_Global_Indicators_New VALUES (
+        12, 0, 0, 3, 
+        3844492519.0, 
+        4171963244.0, 
+        4349844051.0,
+        0, 0, 
+        74.626, 
+        74.992, 
+        NULL, 
+        0, 0, 
+        2.333333333, 
+        2.333333333, 
+        NULL, 
+        0, 0, 
+        1.79, 
+        1.79, 
+        1.79, 
+        'ABW', 12
+    );
+    
+    INSERT INTO Cleaned_Global_Indicators_New VALUES (
+        12, 0, 2, 0, 
+        1550807528479.3704, 
+        1616139282234.0916, 
+        1790503268524.7852,
+        0, 0, 
+        74.626, 
+        74.992, 
+        NULL, 
+        0, 0, 
+        2.333333333, 
+        2.333333333, 
+        NULL, 
+        0, 0, 
+        1.79, 
+        1.79, 
+        1.79, 
+        'ABW', 12
+    );
+    
+    INSERT INTO Cleaned_Global_Indicators_New VALUES (
+        12, 0, 3, 2, 
+        147.0155761, 
+        172.8841711, 
+        169.7982829,
+        0, 0, 
+        74.626, 
+        74.992, 
+        NULL, 
+        0, 0, 
+        2.333333333, 
+        2.333333333, 
+        NULL, 
+        0, 0, 
+        1.79, 
+        1.79, 
+        1.79, 
+        'ABW', 12
+    );
+    
+    INSERT INTO Cleaned_Global_Indicators_New VALUES (
+        12, 0, 1, 1, 
+        22.83170585, 
+        21.20776441, 
+        19.56247345,
+        0, 0, 
+        74.626, 
+        74.992, 
+        NULL, 
+        0, 0, 
+        2.333333333, 
+        2.333333333, 
+        NULL, 
+        0, 0, 
+        1.79, 
+        1.79, 
+        1.79, 
+        'ABW', 12
+    );
+    
+    INSERT INTO Cleaned_Global_Indicators_New VALUES (
+        1, 1, 0, 3, 
+        2805500000000.0, 
+        2908850000000.0, 
+        2977890000000.0,
+        0, 0, 
+        62.44909279, 
+        62.88846323, 
+        NULL, 
+        0, 0, 
+        29.95519379, 
+        29.7372051, 
+        NULL, 
+        0, 0, 
+        759.6552571462319, 
+        805.7603797482622, 
+        857.4685282428658, 
+        'AFE', 1
+    );
+    
+    INSERT INTO Cleaned_Global_Indicators_New VALUES (
+        1, 1, 2, 0, 
+        7.240977656, 
+        10.77375122, 
+        7.126974821,
+        0, 0, 
+        62.44909279, 
+        62.88846323, 
+        NULL, 
+        0, 0, 
+        29.95519379, 
+        29.7372051, 
+        NULL, 
+        0, 0, 
+        759.6552571462319, 
+        805.7603797482622, 
+        857.4685282428658, 
+        'AFE', 1
+    );
+    
+    INSERT INTO Cleaned_Global_Indicators_New VALUES (
+        1, 1, 3, 2, 
+        55.5623958, 
+        58.53616277, 
+        56.87239246,
+        0, 0, 
+        62.44909279, 
+        62.88846323, 
+        NULL, 
+        0, 0, 
+        29.95519379, 
+        29.7372051, 
+        NULL, 
+        0, 0, 
+        759.6552571462319, 
+        805.7603797482622, 
+        857.4685282428658, 
+        'AFE', 1
+    );
+    
+    INSERT INTO Cleaned_Global_Indicators_New VALUES (
+        1, 1, 1, 1, 
+        14.65125488, 
+        14.42979858, 
+        13.92631584,
+        0, 0, 
+        62.44909279, 
+        62.88846323, 
+        NULL, 
+        0, 0, 
+        29.95519379, 
+        29.7372051, 
+        NULL, 
+        0, 0, 
+        759.6552571462319, 
+        805.7603797482622, 
+        857.4685282428658, 
+        'AFE', 1
+    );
+    
+    INSERT INTO Cleaned_Global_Indicators_New VALUES (
+        0, 2, 0, 3, 
+        85767546198.0, 
+        80415503801.0, 
+        82595477072.0,
+        0, 0, 
+        61.982, 
+        62.879, 
+        NULL, 
+        0, 0, 
+        1.852781994, 
+        1.852781994, 
+        NULL, 
+        0, 0, 
+        759.6552571462319, 
+        805.7603797482622, 
+        857.4685282428658, 
+        'AFG', 0
+    );
+    
+    INSERT INTO Cleaned_Global_Indicators_New VALUES (
+        0, 2, 2, 0, 
+        1550807528479.3704, 
+        1616139282234.0916, 
+        1790503268524.7852,
+        0, 0, 
+        61.982, 
+        62.879, 
+        NULL, 
+        0, 0, 
+        1.852781994, 
+        1.852781994, 
+        NULL, 
+        0, 0, 
+        759.6552571462319, 
+        805.7603797482622, 
+        857.4685282428658, 
+        'AFG', 0
+    );
+    
